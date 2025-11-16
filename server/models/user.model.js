d9fabb01f3e03a030bee25fa65b068d8e4a0a973
Hannah Raju <hannahraju96@gmail.com>
2025-11-16 15:54:54 -0500

    finished auth and testing

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose'
+import crypto from'crypto'
+
 const Schema = mongoose.Schema;
 const UserSchema = new mongoose.Schema({
 
@@ -54,14 +56,13 @@ const UserSchema = new mongoose.Schema({
  UserSchema.virtual('password')
  .set(function(password){
     this._password = password;
-   // this.salt = this.makeSalt();
-   // this.hashed_password = this.encryptPassword(password);
-   this.hashed_password = password;
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
  
 })
  .get(function(){
     return this._password;
- });
+ })
 
  UserSchema.path('hashed_password').validate(function(v){
     if(this._password && this._password.length<6){
@@ -71,5 +72,26 @@ const UserSchema = new mongoose.Schema({
         this.invalidate('password', "Password is required");
     }
 },null);
+
+UserSchema.methods = {
+    authenticate: function( plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password
+    },
+
+    encryptPassword: function(password) {
+        if (!password) return ''
+        try {
+            return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex')
+    } catch (err) {
+        return ''
+    }
+    },
+    makeSalt: function() {
+        return Math.round((new Date().valueOf() * Math.random())) + ''
+        }
+}
 
 export default mongoose.model('User', UserSchema)
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import userCtrl from '../controllers/user.controller.js'
+import authCtrl from '../controllers/auth.controller.js'
+
 const router = express.Router()
 router.route('/api/users').post(userCtrl.create)
 router.route('/api/users').get(userCtrl.list)
+router.route('/api/users/:userId').get(authCtrl.requireSignin, userCtrl.read)
+router.route('/api/users/:userId').put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
+router.route('/api/users/:userId').delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove)
 router.param('userId', userCtrl.userByID)
 router.route('/api/users/:userId').get(userCtrl.read)
 router.route('/api/users/:userId').put(userCtrl.update)
