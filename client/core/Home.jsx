63ed3df48d1cb4872e9886d1d34a527a00a1a249
Hannah Raju <hannahraju96@gmail.com>
2025-11-16 19:36:40 -0500

    working on front end

---
+++ client/core/Home.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@ mui/material/CardContent";
+import CardMedia from "@ mui/material/CardMedia";
+import Typography from "@mui/material/T ypography";
+import { useTheme } from "@mui/material/styles";
+import unicornbikeImg from "./../assets/images/unicornbikeImg.jpg";
+
+const Home = () => {
+
+const theme = useTheme();
+return (
+<Card
+sx={{
+maxWidth: 600,
+margin: "auto",
+mt: 5,
+}}
+>
+<Typography
+variant="h2"
+sx={{
+px: 10,
+pt: 3,
+pb: 2,
+color: theme.custom ?.openTitle || theme.palette.primary.main,
+}}
+>
+Home Page
+</Typography>
+<CardMedia
+sx={{ minHeight: 400 }}
+image={unicornbikeImg}
+title= "Unicorn Bike"
+/>
+<CardContent>
+<Typography variant="body2" component="p">
+Welcome to the MERN Skeleton home page.
+</Typography>
+</CardContent>
+</Card>
+);
+};
+export default Home
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from '../models/user.model.js'
 import jwt from 'jsonwebtoken'
 import { expressjwt } from 'express-jwt'
 import config from './../../config/config.js'
+
 const signin = async (req, res) => {
     try {
         let user = await User.findOne({ "email": req.body.email })
